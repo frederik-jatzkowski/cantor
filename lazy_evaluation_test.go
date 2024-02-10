b4cccb9724c49850995b7d3849edd8909eec2bf0
Frederik Jatzkowski <f.l.jatzkowski@gmail.com>
2024-02-10 01:22:59 +0100

    added example

--- lazy_evaluation_test.go
+++ lazy_evaluation_test.go
@@ -1,11 +1,53 @@
 package cantor_test
 
 import (
+	"fmt"
 	"math/rand"
+	"strings"
 	"testing"
 
 	"github.com/frederik-jatzkowski/cantor"
 )
+
+// In cantor, a set can be derived from one or more other sets, eg. the union of multiple sets.
+// Without explicit evaluation, all method results are computed on demand and reflect changes made to their arguments.
+// This allows you to define views (somewhat similar to an SQL VIEW) on changing data, which are composable and usually very performant.
+// Sometimes, it might be good to evaluate such a view into a new ExplicitSet,
+// eg. for performance improvements or to decouple it from its arguments.
+func Example_lazyEvaluation() {
+	birds := cantor.NewExplicitSet("eagle", "pigeon", "duck", "swan")
+	mammals := cantor.NewExplicitSet("lion", "pig", "tiger", "giraffe")
+	fishes := cantor.NewExplicitSet("shark", "barracuda", "goldfish", "guppy")
+
+	animals := birds.Union(mammals).Union(fishes)
+
+	// In this example, we want to search for animals using a search term.
+	search := ""
+
+	// We define an ImplicitSet which contains all strings that start with the search term.
+	startingWithSearchTerm := cantor.NewImplicitSet(func(element string) bool {
+		return strings.HasPrefix(element, search)
+	})
+
+	// We derive a new set, which filters the elements by the search term.
+	result := animals.Intersect(startingWithSearchTerm)
+
+	// Lets search for "pig".
+	search = "pig"
+	fmt.Println(result) // {pigeon, pig}
+
+	// Since result.String() is evaluated on demand, changes in the search term are reflected in our derived set.
+	search = "g"
+	fmt.Println(result) // {giraffe, goldfish, guppy}
+
+	// We can evaluate the result into a new ExplicitSet.
+	evaluated := result.Evaluate()
+
+	// Changes in the search term do not influence this new set.
+	search = "s"
+	fmt.Println(result)    // {swan, shark}
+	fmt.Println(evaluated) // {giraffe, goldfish, guppy}
+}
 
 func BenchmarkIterableSet_Contains(b *testing.B) {
 	// this benchmark should not exceed on a modern CPU:
