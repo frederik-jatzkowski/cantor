25d82d59f52a1cc8c0e72d055b117cc7e60518ee
Frederik Jatzkowski <f.l.jatzkowski@gmail.com>
2024-02-20 12:19:55 +0100

    restructured inner files

---
+++ package_test.go
@@ -0,0 +1,54 @@
+package cantor_test
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/frederik-jatzkowski/cantor"
+)
+
+// In cantor, a set can be derived from one or more other sets.
+// All methods on a derived set are computed just in time and reflect changes made to the underlying sets.
+// This allows you to define views on changing data, which are composable and usually very performant.
+// Sometimes, it might be good to evaluate such a derived set into a new Set.
+// During such evaluation, no intermediate sets must be stored, making the evaluation highly performant
+// and avoiding pressure on the garbage collector.
+func Example_lazyEvaluation() {
+	birds := cantor.NewHashSet("eagle", "pigeon", "duck", "swan")
+	mammals := cantor.NewHashSet("lion", "pig", "tiger", "giraffe")
+	fishes := cantor.NewHashSet("shark", "barracuda", "goldfish", "guppy")
+
+	// We derive a set animals which contains all given birds, mammals and fishes.
+	animals := birds.Union(mammals).Union(fishes)
+
+	// In this example, we want to search for animals using a search term.
+	search := ""
+
+	// We define an ImplicitSet which contains all strings that start with the search term.
+	startingWithSearchTerm := cantor.NewImplicitSet(func(element string) bool {
+		return strings.HasPrefix(element, search)
+	})
+
+	// We derive a new set, which filters the animals by the search term.
+	searchResult := animals.Intersect(startingWithSearchTerm)
+
+	// Lets search for "pig".
+	search = "pig"
+
+	fmt.Println(searchResult) // {pigeon, pig}
+
+	// Since result.String() is evaluated just in time, changes in the search term are reflected in our derived set.
+	search = "g"
+
+	fmt.Println(searchResult) // {giraffe, goldfish, guppy}
+
+	// We can evaluate the result into a new and independent Set.
+	evaluated := searchResult.Evaluate()
+
+	// Changes in the underlying sets of searchResult do not influence this new set.
+	// Additionally, operations on the evaluated set are more performant.
+	mammals.Add("golden retriever")
+
+	fmt.Println(searchResult) // {giraffe, golden retriever, goldfish, guppy}
+	fmt.Println(evaluated)    // {giraffe, goldfish, guppy}
+}
