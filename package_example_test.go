8598b1a696a89153b51049cbdb2723406d8cd4fb
Frederik Jatzkowski <f.l.jatzkowski@gmail.com>
2024-02-21 23:08:02 +0100

    improved examples

--- package_example_test.go
+++ package_example_test.go
@@ -2,55 +2,47 @@ package cantor_test
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/frederik-jatzkowski/cantor"
 )
 
 // In cantor, a set can be derived from one or more other sets.
 // All methods on a derived set are computed just in time and reflect changes made to the underlying sets.
-// This allows you to define views on changing data, which are composable and usually very performant.
-// Sometimes, it might be good to evaluate such a derived set into a new Set.
+// This allows you to define real time views on changing data, which are composable and usually very performant.
+func ExampleDerivedSet_1() {
+	var (
+		birds                             = cantor.NewHashSet("eagle", "pigeon")
+		mammals                           = cantor.NewHashSet("lion", "giraffe")
+		fishes                            = cantor.NewHashSet("shark", "goldfish")
+		animals cantor.DerivedSet[string] = birds.Union(mammals).Union(fishes)
+	)
+
+	fmt.Println(animals) // {pigeon, eagle, lion, giraffe, goldfish, shark}
+
+	mammals.Add("dog")
+
+	// The derived set animals reflects the change of the underlying set mammals.
+	fmt.Println(animals) // {pigeon, eagle, lion, giraffe, dog, shark, goldfish}
+}
+
+// Sometimes, it might be beneficial to evaluate such a DerivedSet into an independent Set.
 // During such evaluation, no intermediate sets must be stored, making the evaluation highly performant
 // and avoiding pressure on the garbage collector.
-func Example_derivedSets() {
-	birds := cantor.NewHashSet("eagle", "pigeon", "duck", "swan")
-	mammals := cantor.NewHashSet("lion", "pig", "tiger", "giraffe")
-	fishes := cantor.NewHashSet("shark", "barracuda", "goldfish", "guppy")
+// This is possible due to evaluation of boolean expressions under the hood.
+func ExampleDerivedSet_2() {
+	var (
+		birds   = cantor.NewHashSet("eagle", "pigeon")
+		mammals = cantor.NewHashSet("lion", "giraffe")
+		fishes  = cantor.NewHashSet("shark", "goldfish")
+		animals = birds.Union(mammals).Union(fishes).IntoHashSet()
+	)
 
-	// We derive a set animals which contains all given birds, mammals and fishes.
-	animals := birds.Union(mammals).Union(fishes)
+	fmt.Println(animals) // {pigeon, eagle, lion, giraffe, goldfish, shark}
 
-	// In this example, we want to search for animals using a search term.
-	search := ""
+	mammals.Add("dog")
 
-	// We define an ImplicitSet which contains all strings that start with the search term.
-	startingWithSearchTerm := cantor.NewImplicitSet(func(element string) bool {
-		return strings.HasPrefix(element, search)
-	})
-
-	// We derive a new set, which filters the animals by the search term.
-	searchResult := animals.Intersect(startingWithSearchTerm)
-
-	// Lets search for "pig".
-	search = "pig"
-
-	fmt.Println(searchResult) // {pigeon, pig}
-
-	// Since result.String() is evaluated just in time, changes in the search term are reflected in our derived set.
-	search = "g"
-
-	fmt.Println(searchResult) // {giraffe, goldfish, guppy}
-
-	// We can evaluate the result into a new and independent Set.
-	evaluated := searchResult.IntoHashSet()
-
-	// Changes in the underlying sets of searchResult do not influence this new set.
-	// Additionally, operations on the evaluated set are more performant.
-	mammals.Add("golden retriever")
-
-	fmt.Println(searchResult) // {giraffe, golden retriever, goldfish, guppy}
-	fmt.Println(evaluated)    // {giraffe, goldfish, guppy}
+	// Since we evaluated all animals into a hash set, changes to the set mammals are not reflected anymore.
+	fmt.Println(animals) // {pigeon, eagle, lion, giraffe, goldfish, shark}
 }
 
 // Before the implementation of go rangefuncs, you can use a FunctionIterator like this.
